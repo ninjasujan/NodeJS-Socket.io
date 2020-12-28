bd71cde55c21dfdbd62858b94bdb57a9d3688cf9
sujanpoojary <sujanpoojary884@gmail.com>
2020-12-28 09:20:25 +0530

    user chatroom and name parsed

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,25 @@
 const socket = io();
 const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
+// get username and chat room
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+// emit the event when user selects the chat room and enter into the chat
+socket.emit("chatRoom", { username, room });
 
 // function to manipulate the DOM to add chat message in UI
 const outputToDOM = (message) => {
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+  div.innerHTML = `<p class="meta">${username} <span>${message.time}</span></p>
             <p class="text">
-              ${message}
+              ${message.text}
             </p>`;
   document.querySelector(".chat-messages").appendChild(div);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 };
 
 socket.on("message", (message) => {
@@ -21,4 +31,6 @@ chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const chatMessage = e.target.elements.msg.value;
   socket.emit("chat", chatMessage);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
--- server.js
+++ server.js
@@ -5,27 +5,40 @@ const http = require("http");
 const socketio = require("socket.io");
 require("dotenv").config();
 
+/* msg formater */
+const messageFormat = require("./Utils/message");
+
 const server = http.createServer(app);
 const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const boatName = "Chat Boat";
+
 io.on("connection", (socket) => {
-  // welcome the user
-  socket.emit("message", "Welcome to chat board.");
-  // notify to all user when the user has connected.
-  socket.broadcast.emit("message", "A user has joined the chat.!");
+  // selection of chat room event
+  socket.on("chatRoom", ({ username, room }) => {
+    // welcome the user
+    socket.emit(
+      "message",
+      messageFormat(username, "Welcome to the chat board.!")
+    );
+    // notify to all user when the user has connected.
+    socket.broadcast.emit(
+      "message",
+      messageFormat(username, "user has joined the chat")
+    );
+  });
 
   // chat sent by client
   socket.on("chat", (chatMessage) => {
-    console.log("chat message by user", chatMessage);
-    io.emit("message", chatMessage);
+    io.emit("message", messageFormat("User claira", chatMessage));
   });
 
   // disconnect from chat
   socket.on("disconnect", () => {
-    io.emit("message", "user has left the chat.!");
+    io.emit("message", messageFormat("User claira", "user has left the chat"));
   });
 });
 
